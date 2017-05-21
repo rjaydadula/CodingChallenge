b8304f6f8836aaafc1e3d6ef51f28e45175ed87b
rjaydadula <rjaydadula@gmail.com>
2017-05-21 20:36:19 +0700

    1. Creating a ChallengeMap 2. Populating Data to ChallengeMap 3. Display Map Data

--- CodingChallenge/Program.cs
+++ CodingChallenge/Program.cs
@@ -8,8 +8,40 @@ namespace CodingChallenge
 {
     class Program
     {
+        private static int mapMaxCountX = 10;
+        private static int mapMaxCountY = 10;
+
         static void Main(string[] args)
         {
+            int[,] challengeMap = new int[mapMaxCountX, mapMaxCountY];
+            int valuePerBox = 1500;
+
+            //Populating the ChallengeMap with data
+            for (int mapIndex_X = 0; mapIndex_X < mapMaxCountX; mapIndex_X++)
+            {
+                for (int mapIndex_Y = 0; mapIndex_Y < mapMaxCountY; mapIndex_Y++)
+                {
+                    valuePerBox--;
+
+                    if (valuePerBox == 0)
+                        valuePerBox = 1499;
+
+                    challengeMap[mapIndex_X, mapIndex_Y] = valuePerBox;
+                }
+            }
+
+            //DISPLAYING MAP
+            for (int mapIndex = 0; mapIndex < challengeMap.GetLength(0); mapIndex++)
+            {
+                for (int mapctrPerIndex = 0; mapctrPerIndex < challengeMap.GetLength(1); mapctrPerIndex++)
+                {
+                    Console.Write(" " + challengeMap[mapIndex, mapctrPerIndex].ToString() + " ");
+
+                }
+                Console.Write("\n");
+            }
+
+
         }
     }
 }
