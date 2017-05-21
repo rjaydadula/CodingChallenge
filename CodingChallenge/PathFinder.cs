8779139a31b2f2f234e638b930bb4f348ced5ed3
rjaydadula <rjaydadula@gmail.com>
2017-05-21 20:58:05 +0700

    1. Added New Class PathFinder 2. Added New Class Node 3. Populating PathFinder with data in ChallengeMap

---
+++ CodingChallenge/PathFinder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallenge
+{
+    public class Node
+    {
+        public int east { get; set; }
+        public int west { get; set; }
+        public int south { get; set; }
+    }
+
+    public class PathFinder
+    {
+        public Dictionary<int, List<Node>> nodeListPerIndex { get; set; }
+        public int LengthOfCalculatedPath { get; set; }
+        public int DropOfCalculatedPath { get; set; }
+        public string CalculatedPath { get; set; }
+    }
+}
--- CodingChallenge/Program.cs
+++ CodingChallenge/Program.cs
@@ -16,6 +16,8 @@ namespace CodingChallenge
             int[,] challengeMap = new int[mapMaxCountX, mapMaxCountY];
             int valuePerBox = 1500;
 
+            PathFinder pathFinder = new PathFinder();
+
             //Populating the ChallengeMap with data
             for (int mapIndex_X = 0; mapIndex_X < mapMaxCountX; mapIndex_X++)
             {
@@ -31,16 +33,59 @@ namespace CodingChallenge
             }
 
             //DISPLAYING MAP
-            for (int mapIndex = 0; mapIndex < challengeMap.GetLength(0); mapIndex++)
+            for (int mapIndex_X = 0; mapIndex_X < mapMaxCountX; mapIndex_X++)
             {
-                for (int mapctrPerIndex = 0; mapctrPerIndex < challengeMap.GetLength(1); mapctrPerIndex++)
+                for (int mapIndex_Y = 0; mapIndex_Y < mapMaxCountY; mapIndex_Y++)
                 {
-                    Console.Write(" " + challengeMap[mapIndex, mapctrPerIndex].ToString() + " ");
+                    Console.Write(" " + challengeMap[mapIndex_X, mapIndex_Y].ToString() + " ");
 
                 }
                 Console.Write("\n");
             }
 
+
+            pathFinder.nodeListPerIndex = new Dictionary<int, List<Node>>();
+
+            for (int mapIndex_X = 0; mapIndex_X < mapMaxCountX; mapIndex_X++)
+            {
+                for (int mapIndex_Y = 0; mapIndex_Y < mapMaxCountY; mapIndex_Y++)
+                {
+                    Node node = new Node();
+
+                    if (mapIndex_Y == 0)
+                        node.east = 1500;
+                    else
+                        node.east = challengeMap[mapIndex_X, mapIndex_Y - 1];
+
+
+                    if (mapIndex_Y == mapMaxCountY - 1)
+                        node.west = 1500;
+                    else
+                        node.west = challengeMap[mapIndex_X, mapIndex_Y + 1];
+
+
+                    if (mapIndex_X == mapMaxCountX - 1)
+                        node.south = 1500;
+                    else
+                        node.south = challengeMap[mapIndex_X + 1, mapIndex_Y];
+
+
+                    if (!pathFinder.nodeListPerIndex.ContainsKey(mapIndex_X))
+                        pathFinder.nodeListPerIndex.Add(mapIndex_X, new List<Node>() { node });
+                    else
+                        pathFinder.nodeListPerIndex[mapIndex_X].Add(node);
+
+                }
+            }
+
+
+
+            Console.Write("\n\n");
+            Console.WriteLine("Length Of Calculated Path: "+ pathFinder.LengthOfCalculatedPath);
+            Console.WriteLine("Drop Of Calculated Path: " + pathFinder.DropOfCalculatedPath);
+            Console.WriteLine("Calculated Path: " + pathFinder.CalculatedPath);
+
+            Console.Read();
 
         }
     }
